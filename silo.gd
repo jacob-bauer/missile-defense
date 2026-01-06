117edbf737a0403b658cb4306a4d4e65d81b1097
Jacob Bauer <billybobauer@gmail.com>
2026-01-06 11:37:23 -0700

    Remove unneccessary signal from silo.gd

--- silo.gd
+++ silo.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
 
-signal missile_launched
-
-
 @export var missile_prototype: PackedScene = preload("res://missile.tscn")
 @export var missile_quantity: int = 10:
 	set(value):
@@ -23,5 +20,3 @@ func launch(target_position: Vector2) -> void:
 		var active_missile: Missile = missile_prototype.instantiate()
 		playable_area.add_child(active_missile)
 		active_missile.launch(position, target_position)
-		
-		missile_launched.emit()
