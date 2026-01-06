ca5ba26e3e5adbb656297febec64b015920875a2
Jacob Bauer <billybobauer@gmail.com>
2026-01-06 16:17:46 -0700

    Adjust Silo to have a marker that represents the target position

--- silo.gd
+++ silo.gd
@@ -24,4 +24,5 @@ func launch(target_position: Vector2) -> void:
 		
 		var active_missile: Missile = missile_prototype.instantiate()
 		playable_area.add_child(active_missile)
-		active_missile.launch(position, target_position)
+		print($LaunchPosition.position)
+		active_missile.launch($LaunchPosition.position, target_position)
