05f4ad2a37ae978b3c3a63fa33eb839945fcf728
Jacob Bauer <billybobauer@gmail.com>
2026-01-08 19:32:28 -0700

    Find bug in silo selection code and fix it. Update TODO.MD

--- playable_area.gd
+++ playable_area.gd
@@ -1,28 +1,36 @@
 extends Node2D
 
 
+var _silos: Array[Silo]
+
+
 @export var game_state: GameData
 
 
 func _ready() -> void:
 	game_state._reset_state()
-
+	var silo_nodes: Array[Node] = get_tree().get_nodes_in_group("silos") as Array[Node]
+	_silos.assign(silo_nodes)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			# Figure out which silo is closest to the mouse position
-			# that also has any remaining missiles.
-			# Launch from that silo.
-			var silo_nodes: Array[Node] = get_tree().get_nodes_in_group("silos") as Array[Node]
-			var silos: Array[Silo] = []
-			silos.assign(silo_nodes)
+			# First, check if any silos are empty. If so, remove them
+			if _silos.size() > 1:
+				var indices_to_remove: Array[int] = []
+				for i in range(_silos.size()):
+					if _silos[i].missile_quantity <= 0:
+						indices_to_remove.append(i)
+					
+				for i in indices_to_remove:
+					_silos.remove_at(i)
 			
+			# Now _silos only contains valid silos. Select the closest one
 			var mouse_position: Vector2 = get_global_mouse_position()
-			var closest_silo = silos[0]
-			for silo in silos:
-				if silo.position.distance_to(mouse_position) < closest_silo.position.distance_to(mouse_position) \
-					and silo.missile_quantity > 0:
-						closest_silo = silo
+			var closest_silo = _silos[0]
+			for silo in _silos:
+				if silo.position.distance_to(mouse_position) < closest_silo.position.distance_to(mouse_position):
+					closest_silo = silo
 			
 			closest_silo.launch(mouse_position)
+			
