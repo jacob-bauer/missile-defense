b37ec19f3b14068bad13ec5e206a0a9b8b9bb0b0
Jacob Bauer <billybobauer@gmail.com>
2026-01-08 19:53:28 -0700

    Refactor silo selection code into a couple of well named functions.
    This also prepares us to enable firing off a signal when the user runs
    out of missiles. How do we hook our silos into game_data?

--- playable_area.gd
+++ playable_area.gd
@@ -9,6 +9,7 @@ var _silos: Array[Silo]
 
 func _ready() -> void:
 	game_state._reset_state()
+	
 	var silo_nodes: Array[Node] = get_tree().get_nodes_in_group("silos") as Array[Node]
 	_silos.assign(silo_nodes)
 
@@ -16,21 +17,28 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			# First, check if any silos are empty. If so, remove them
-			if _silos.size() > 1:
-				var indices_to_remove: Array[int] = []
-				for i in range(_silos.size()):
-					if _silos[i].missile_quantity <= 0:
-						indices_to_remove.append(i)
-					
-				for i in indices_to_remove:
-					_silos.remove_at(i)
-			
-			# Now _silos only contains valid silos. Select the closest one
-			var mouse_position: Vector2 = get_global_mouse_position()
-			var closest_silo = _silos[0]
-			for silo in _silos:
-				if silo.position.distance_to(mouse_position) < closest_silo.position.distance_to(mouse_position):
-					closest_silo = silo
-			
-			closest_silo.launch(mouse_position)
+			fire_closest_silo(get_global_mouse_position())
+
+
+func get_silos_with_missiles() -> Array[Silo]:
+	var silos_with_missiles: Array[Silo] = []
+	for silo in _silos:
+		if silo.missile_quantity > 0:
+			silos_with_missiles.append(silo)
+	
+	return silos_with_missiles
+
+
+func fire_closest_silo(target_position: Vector2) -> bool:
+	var silos_with_missiles: Array[Silo] = get_silos_with_missiles()
+	if silos_with_missiles.size() > 0:
+		var closest_silo: Silo = silos_with_missiles[0]
+		for silo in silos_with_missiles:
+			if silo.position.distance_to(target_position) < closest_silo.position.distance_to(target_position):
+				closest_silo = silo
+		
+		closest_silo.launch(target_position)
+		return true
+		
+	else:
+		return false
