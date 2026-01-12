8931c2bd0c88c1122ed12cdbab395212df84487e
Jacob Bauer <billybobauer@gmail.com>
2026-01-12 12:53:23 -0700

    Prepare for converting to manual animation

--- new_missile.gd
+++ new_missile.gd
@@ -7,6 +7,18 @@ var _target_position: Vector2
 
 ## Speed in pixels per second of flight to target
 @export var flight_speed: int = 240
+## The radius in pixels of the missile explosion at it's largest
+@export var explosion_radius: int = 40
+## The number of seconds the explosion takes. It will reach it's peak explosion_radius
+## at [code]explosion_lifetime / 2[/code] seconds.
+@export var explosion_lifetime: float = 3
+
+
+func _draw() -> void:
+	# Now I need this to be animated WITH the size of the collision shape.
+	# Unfortunately this means I cannot use the animation player, and so must also manually
+	# animate the collision shape
+	draw_circle(Vector2.ZERO, 20, Color.WHITE, -1, true)
 
 
 func launch(silo_position: Vector2, target_position: Vector2) -> void:
@@ -19,6 +31,7 @@ func launch(silo_position: Vector2, target_position: Vector2) -> void:
 
 func _on_target_reached() -> void:
 	$CollisionShape2D.disabled = false
-	position = _target_position 
+	position = _target_position
+	queue_redraw()
 	$AnimationPlayer.play("Explode")
 	
