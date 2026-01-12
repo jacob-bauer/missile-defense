6f5798ec32f34b1cd999137614a4d40d20bffab1
Jacob Bauer <billybobauer@gmail.com>
2026-01-12 16:28:18 -0700

    Renamed NewMissile to Missile and removed the old Missile

--- missile.gd
+++ missile.gd
@@ -1,33 +1,51 @@
-extends Node2D
+extends Area2D
 class_name Missile
 
 
-var _active_warhead: Explosion
 var _target_position: Vector2
+var _current_radius: float:
+	set(value):
+		_current_radius = value
+		queue_redraw()
+	get:
+		return _current_radius
 
-
-@export var inactive_warhead: PackedScene = preload("res://explosion.tscn")
-@export var smoke: PackedScene = preload("res://smoke_trail.tscn")
 
 ## Speed in pixels per second of flight to target
 @export var flight_speed: int = 240
+## The radius in pixels of the missile explosion at it's largest
+@export var explosion_radius: int = 20
+## The number of seconds the explosion takes. It will reach it's peak explosion_radius
+## at [code]explosion_lifetime / 2[/code] seconds.
+@export var explosion_lifetime: float = 3
+
+
+func _ready() -> void:
+	set_process(false)
+	$CollisionShape2D.disabled = true
+	$CollisionShape2D.shape.radius = 0
+
+
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, _current_radius, Color.WHITE, true, -1, true)
 
 
 func launch(silo_position: Vector2, target_position: Vector2) -> void:
-	var active_warhead: Explosion = inactive_warhead.instantiate()
-	active_warhead.name = "ActiveWarhead"
-	add_child(active_warhead)
-	
-	_active_warhead = active_warhead
 	_target_position = target_position
+	$CollisionShape2D.disabled = true
 	
 	var time_of_flight: float = silo_position.distance_to(target_position) / flight_speed
-	var active_smoke: SmokeTrail = smoke.instantiate()
-	active_smoke.target_reached.connect(_on_target_reached)
-	add_child(active_smoke)
-	active_smoke.launch(silo_position, target_position, time_of_flight)
+	$SmokeTrail.launch(silo_position, target_position, time_of_flight)
 
 
 func _on_target_reached() -> void:
+	$CollisionShape2D.disabled = false
 	position = _target_position
-	_active_warhead.explode()
+	
+	var explosion_growth = get_tree().create_tween()
+	explosion_growth.set_parallel()
+	explosion_growth.tween_property($CollisionShape2D.shape, "radius", explosion_radius, explosion_lifetime / 2)
+	explosion_growth.tween_property(self, "_current_radius", explosion_radius, explosion_lifetime / 2)
+	explosion_growth.chain()
+	explosion_growth.tween_property($CollisionShape2D.shape, "radius", 0, explosion_lifetime / 2)
+	explosion_growth.tween_property(self, "_current_radius", 0, explosion_lifetime / 2)
--- silo.gd
+++ silo.gd
@@ -12,7 +12,7 @@ var enemies_should_target_here: Vector2:
 		return $TargetPosition.global_position
 
 
-@export var missile_prototype: PackedScene = preload("res://new_missile.tscn")
+@export var missile_prototype: PackedScene = preload("res://missile.tscn")
 @export var missile_speed: int = 240
 @export var missile_quantity: int = 10:
 	set(value):
@@ -36,7 +36,7 @@ func launch(target_position: Vector2, launch_position: Vector2 = $LaunchPosition
 	if missile_quantity > 0:
 		missile_quantity -= 1
 		
-		var active_missile: NewMissile = missile_prototype.instantiate()
+		var active_missile: Missile = missile_prototype.instantiate()
 		active_missile.flight_speed = missile_speed
 		playable_area.add_child(active_missile)
 		active_missile.launch(launch_position, target_position)
