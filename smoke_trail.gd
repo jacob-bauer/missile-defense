1017e3f07e8b44ce9829a7157beca1916e4b4980
Jacob Bauer <billybobauer@gmail.com>
2026-01-12 16:23:05 -0700

    Rework SmokeTrail to take advantage of tweening
    SmokeTrail now takes advantage of tweening to do it's animation.
    This is less code, runs smoother, is easier to maintain, and has
    better performance.

--- smoke_trail.gd
+++ smoke_trail.gd
@@ -5,34 +5,26 @@ class_name SmokeTrail
 signal target_reached
 
 
-var _target_position: Vector2
-var _speed: int
-var _direction: Vector2
+# This variable is tweened and that causes our line to be animated
+@warning_ignore("unused_private_class_variable")
+var _current_line_end: Vector2:
+	set(value):
+		add_point(value, 1)
+	get:
+		return points[1]
 
 
 func _ready() -> void:
-	set_process(false)
-
-
-func _process(delta: float) -> void:
-	var velocity = _direction * _speed * delta
-	var new_position = points[1] + velocity
-	
-	if new_position.distance_to(_target_position) > points[1].distance_to(_target_position):
-		set_process(false)
-		queue_free()
-		target_reached.emit()
-		
-	else:
-		set_point_position(1, new_position)
+	antialiased = true
 
 
 func launch(launch_position: Vector2, target_position: Vector2, time_of_flight: float) -> void:
-	_target_position = target_position
-	_speed = roundi(launch_position.distance_to(target_position) / time_of_flight)
-	_direction = launch_position.direction_to(target_position)
-	
 	add_point(launch_position, 0)
 	add_point(launch_position, 1)
 	
-	set_process(true)
+	var line_length = get_tree().create_tween()
+	line_length.tween_property(self, "_current_line_end", target_position, time_of_flight)
+	
+	await line_length.finished
+	queue_free()
+	target_reached.emit()
