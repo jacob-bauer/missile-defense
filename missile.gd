31e75b29a8d9f440a13ef355ab3aeb60f72f3e1c
Jacob Bauer <billybobauer@gmail.com>
2026-01-24 23:44:41 -0700

    Fix bug where enemy missiles don't detect enemy missile explosions
    It was a simple error in setting the collision mask

--- missile.gd
+++ missile.gd
@@ -75,7 +75,7 @@ func set_initial_physics_layers() -> void:
 		collision_mask = GameData.Collision_Layers.ENEMY_MISSILES
 	else:
 		collision_layer = GameData.Collision_Layers.ENEMY_MISSILES
-		collision_mask = GameData.Collision_Layers.FRIENDLY_MISSILES | GameData.Collision_Layers.CITY | GameData.Collision_Layers.FRIENDLY_MISSILES
+		collision_mask = GameData.Collision_Layers.FRIENDLY_MISSILES | GameData.Collision_Layers.CITY | GameData.Collision_Layers.ENEMY_MISSILES
 
 
 func _on_target_reached() -> void:
@@ -103,6 +103,7 @@ func _on_target_reached() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if not _friendly and not _exploded and area is Missile:
+		var missile = area as Missile
 		_exploded = true
 		_target_position = $CollisionShape2D.position
 		_on_target_reached()
