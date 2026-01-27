d3e349e1b90603d80f635313286f87df83da2666
Jacob Bauer <billybobauer@gmail.com>
2026-01-27 11:45:20 -0700

    Main Menu logs when loading in or when changing scenes

--- main_menu.gd
+++ main_menu.gd
@@ -1,8 +1,24 @@
 extends Control
 
 
+var _log_ready: String =\
+GameData.log_separator +\
+"""Game Loaded
+DateTime:\t{datetime}"""
+
+var _log_new_scene: String =\
+GameData.log_separator +\
+"""Changing to new Scene
+Scene:\t{scene}"""
+
+
 var game_scene: PackedScene = preload("res://game.tscn")
 
 
+func _ready() -> void:
+	print(_log_ready.format({"datetime":Time.get_datetime_string_from_system()}))
+
+
 func _on_play_pressed() -> void:
+	print(_log_new_scene.format({"scene":"res://game.tscn"}))
 	get_tree().change_scene_to_packed(game_scene)
