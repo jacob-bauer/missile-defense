467107f9844ed1b342c84333f8cc8989af4e54ef
Jacob Bauer <billybobauer@gmail.com>
2026-01-27 11:37:58 -0700

    Log CityBlock events

--- city_block.gd
+++ city_block.gd
@@ -2,6 +2,18 @@ extends Area2D
 class_name CityBlock
 
 
+var _log_damage: String =\
+GameData.log_separator +\
+"""CityBlock Damaged
+Name:\t{name}
+New Health:\t{health}"""
+
+var _log_destroyed: String =\
+GameData.log_separator +\
+"""CityBlock Destroyed
+Name:\t{name}"""
+
+
 signal destroyed
 
 
@@ -54,5 +66,9 @@ func reduce_health() -> void:
 	_decrement_health()
 	
 	if _health == 0:
+		print(_log_destroyed.format({"name":get_path()}))
 		$FullHealthPolygon.set_deferred("disabled", true)
 		destroyed.emit()
+	else:
+		print_verbose(_log_damage.format({"name":get_path,
+										  "health":_health}))
