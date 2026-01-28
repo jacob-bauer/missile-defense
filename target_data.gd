43803c63fdddad69c92259ecfbadd69f0979476a
Jacob Bauer <billybobauer@gmail.com>
2026-01-27 17:37:16 -0700

    TargetData type now exists
    This type exists to be a data holder for a dictionary entry of the
    form <node>:<TargetData> and will be useful for managing where the
    enemy should target

---
+++ target_data.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name TargetData
+
+
+var global_position: Vector2
+var enabled: bool
+
+
+@warning_ignore("shadowed_variable")
+func _init(global_position: Vector2, enabled: bool) -> void:
+	global_position = global_position
+	enabled = enabled
