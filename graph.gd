750332d24e7658af45d4d2893f0b67b9e175474e
Jacob Bauer <billybobauer@gmail.com>
2026-01-30 15:33:41 -0700

    There is now a basic graphing utility

--- graph.gd
+++ graph.gd
@@ -10,4 +10,13 @@ func _ready() -> void:
 	while not file.eof_reached():
 		lines.append(file.get_csv_line(","))
 	
-	print(lines)
+	var max_score: int = 0
+	for line in lines:
+		if (line.size() > 1) and (int(line[2]) > max_score):
+			max_score = int(line[2])
+	
+	print(max_score)
+	for i in range(0, lines.size() - 1):
+		var percent_complete: float = float(i) / lines.size()
+		var percent_height: float = float(lines[i][2]) / max_score
+		$Line2D.add_point(Vector2(get_viewport().size.x * percent_complete, get_viewport().size.y * percent_height))
